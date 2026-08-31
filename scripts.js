f6d85699e11cc7295523a99754188cf74bdeba3e
Enrico <enricokuhndemoraespires@gmail.com>
2026-08-31 17:10:02 -0300

    Adicionado script para o cabeçalho

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("header");
+
+function handleScroll() {
+  if (window.scrollY > 24) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+}
+handleScroll();
+window.addEventListener("scroll", handleScroll, { passive: true });
